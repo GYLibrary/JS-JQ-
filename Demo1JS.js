75e8fc1f7488a922793ba01d87195809c3ddf471
airfight <745756619@qq.com>
2016-10-30 22:03:22 +0800

    JS

--- Demo1JS.js
+++ Demo1JS.js
@@ -1,3 +1,15 @@
 /**
  * Created by giant on 16/10/29.
  */
+
+ var msg= '<h2>browes window</h2><p>width: ' + window.innerWidth + '</p>'
+ msg = '<p>height: ' + window.innerHeight + '</p>'
+
+ msg = '<h2>history</h2><p> item: ' + window.history.length + '</p>'
+ msg = '<h2>screen</h2> width:' + window.screen.width + '</p>'
+ msg = '<p>height:' + window.screen.height + '</p>'
+
+ var e1 = document.getElementById('info')
+ e1.innerHTML = msg
+
+ alert('Current Page:' + window.location)
